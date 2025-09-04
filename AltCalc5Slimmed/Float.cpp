78100b9498884c1392b37fe6de15808f0aab724d
Thomas DiModica <ricinwich@yahoo.com>
2025-09-03 22:22:21 -0600

    And fix this here.

--- AltCalc5Slimmed/Float.cpp
+++ AltCalc5Slimmed/Float.cpp
@@ -474,7 +474,7 @@ namespace BigInt
        }
       else
        {
-         temp = lhs.Exponent > rhs.Exponent ? lhs : rhs;
+         temp = lhs.Exponent > rhs.Exponent ? lhs : -rhs;
        }
 
       return temp;
